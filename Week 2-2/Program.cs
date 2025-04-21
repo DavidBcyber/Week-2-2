81c7b4be8703bb29ca4fab985ccd5675e1a6be73
xbatm <dbustos28@amarillocollege.com>
2025-04-20 22:26:38 -0500

    Add project files.

---
+++ Week 2-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Week_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length, width;
+            Console.WriteLine("Enter the length of the rectangle: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Width of the rectangle: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Result:");
+            Console.WriteLine("Area of the rectangle: " + (length * width));
+            Console.WriteLine("Perimenter of the rectangle: " + (length + length + width + width));
+
+            Console.ReadLine();
+        }
+    }
+}
